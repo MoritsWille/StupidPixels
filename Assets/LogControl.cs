4cfef1f2e2e2acabcae3f4fdfd99a13d7732f4dc
Morits Wille <moritsbw@live.dk>
2016-01-20 10:52:00 +0100

    Added log obstacles

--- Assets/LogControl.cs
+++ Assets/LogControl.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class LogControl : MonoBehaviour {
     public Sprite Fall;
+    public Transform Player;
+    public Transform Log1;
+    public Transform Log2;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +14,17 @@ public class LogControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if (Player.position.y - 15 > transform.position.y)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + Random.Range(75, 150),0);
+        if (Log1.position.y + 5 < transform.position.y || Log1.position.y - 5 > transform.position.y)
+        {
+            if (Log2.position.y + 5 < transform.position.y || Log2.position.y - 5 > transform.position.y)
+            {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Random.Range(30, 100), 0);
+            }
+        }
+    }
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -8,7 +8,7 @@ public class PlayerControl : MonoBehaviour {
     public Sprite WalkLeftS;
     public Sprite WalkRightS;
     public Sprite Still;
-    bool Brake;
+    bool Dead = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,50 +17,62 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (SpeedperTick > 0.001f)
+        if (Dead)
         {
-            SpeedperTick = SpeedperTick - 0.0075f;
+
         }
-        else SpeedperTick = 0;
-
-        transform.position = new Vector3(transform.position.x, transform.position.y + SpeedperTick, 0);
-
-	    if (Input.GetKeyDown("w"))
+        else
         {
-            if (WalkRight == true)
+            if (SpeedperTick > 0.001f)
             {
-                gameObject.GetComponent<SpriteRenderer> ().sprite = WalkLeftS;
-                WalkRight = false;
+                SpeedperTick = SpeedperTick - 0.0050f;
             }
-            else
+            else SpeedperTick = 0;
+
+            transform.position = new Vector3(transform.position.x, transform.position.y + SpeedperTick, 0);
+
+            if (Input.GetKeyDown("w"))
             {
-                gameObject.GetComponent<SpriteRenderer>().sprite = WalkRightS;
-                WalkRight = true;
+                if (WalkRight == true)
+                {
+                    gameObject.GetComponent<SpriteRenderer>().sprite = WalkLeftS;
+                    WalkRight = false;
+                }
+                else
+                {
+                    gameObject.GetComponent<SpriteRenderer>().sprite = WalkRightS;
+                    WalkRight = true;
+                }
+
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, 0);
             }
 
-            transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, 0);
-        }
-            
-        if (Input.GetKeyDown("a"))
-        {
-            if (transform.position.x == 0 || transform.position.x == 12.5f)
-          transform.position = new Vector3(transform.position.x - 12.5f, transform.position.y, 0);
-        }
+            if (Input.GetKeyDown("a"))
+            {
+                if (transform.position.x == 0 || transform.position.x == 12.5f)
+                    transform.position = new Vector3(transform.position.x - 12.5f, transform.position.y, 0);
+            }
 
-        if (Input.GetKeyDown("d"))
-        {
-            if (transform.position.x == 0 || transform.position.x == -12.5f)
-                transform.position = new Vector3(transform.position.x + 12.5f, transform.position.y, 0);
+            if (Input.GetKeyDown("d"))
+            {
+                if (transform.position.x == 0 || transform.position.x == -12.5f)
+                    transform.position = new Vector3(transform.position.x + 12.5f, transform.position.y, 0);
+            }
+
+            SpeedperTick = transform.position.y - BeforePos;
+
+            BeforePos = transform.position.y;
         }
-
-        SpeedperTick = transform.position.y - BeforePos;
-
-        BeforePos = transform.position.y;
 	}
 
     void OnCollisionEnter2D()
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = null;
+        Dead = true;
+    }
+
+    void DeadText ()
+    {
+
     }
 }
